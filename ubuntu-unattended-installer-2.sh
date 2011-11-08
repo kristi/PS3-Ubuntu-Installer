9c1129d087b6c788a958cc8f8afee4ff40d358db
kristi <kristi.dev@gmail.com>
2011-11-08 02:22:48 -0800

    Add unattended installers

---
+++ ubuntu-unattended-installer-2.sh
@@ -0,0 +1,180 @@
+#!/bin/bash
+
+############# Section 2 of Ubuntu Installer ###########
+## Had to add this section due to chrooting process. ##
+#######################################################
+
+
+## Adding color to terminal
+
+export TERM=xterm-color
+
+###################### End of Chrooting Process ######################
+
+
+
+## Setting up fstab
+
+echo " "
+echo "Setting up fstab entries. . ."
+echo " "
+echo -e "/dev/ps3dd2	/		extextvar	defaults		0 1\n/dev/ps3vram	none		swap	sw			0 0\n/dev/ps3dd1	none		swap	sw			0 0\n/dev/sr0	/mnt/cdrom	auto	noauto,ro		0 0\nproc		/proc		proc	defaults		0 0\nshm		/dev/shm	tmpfs	nodev,nosuid,noexec	0 0\ndev	/dev	tmpfs	rw	0 0" > /etc/fstab
+
+
+## Setting up timezone
+
+echo "Setting up timezone data"
+echo " "
+dpkg-reconfigure tzdata
+
+
+## Configuring Network Data
+
+read -p "Please enter the name of your Playstation 3 Ubuntu Box. (No spaces or odd characters): " D
+echo " "
+echo "Saving $D into /etc/hostname"
+echo $D > /etc/hostname
+touch /etc/hosts
+echo "127.0.0.1		localhost" > /etc/hosts
+
+## User creation and password setting
+echo "Starting user creation and password entries..."
+
+echo "Please set a new root password."
+passwd
+echo " "
+
+## Adding new base user
+read -p "Please enter in a username you would like to use: " F
+if [ "$F" = "" ]; then
+	echo "That username was not valid"
+else
+	echo "Creating user $F"
+	adduser $F
+fi 
+
+## Adding user to admin group
+echo "Adding $F to admin group"
+usermod -aG sudo $F
+
+## Setting up /etc/network/interfaces
+
+echo " "
+echo "Setting up network interfaces"
+echo " "
+echo -e "auto lo\niface lo inet loopback\n\nauto eth0\niface eth0 inet dhcp\n" > /etc/network/interfaces
+
+
+## Configuring apt-get sources in /etc/apt/sources.list
+
+echo " "
+echo "Creating entries for sources.list"
+echo " "
+echo -e "deb http://ports.ubuntu.com/ubuntu-ports/ natty main restricted\ndeb-src http://ports.ubuntu.com/ubuntu-ports/ natty-updates restricted\ndeb http://ports.ubuntu.com/ubuntu-ports/ natty universe\ndeb http://ports.ubuntu.com/ubuntu-ports/ natty-updates universe\ndeb http://ports.ubuntu.com/ubuntu-ports/ natty multiverse\ndeb http://ports.ubuntu.com/ubuntu-ports/ natty-updates multiverse\ndeb http://ports.ubuntu.com/ubuntu-ports/ natty-security main restricted\ndeb-src http://ports.ubuntu.com/ubuntu-ports/ natty-security main restricted\ndeb http://ports.ubuntu.com/ubuntu-ports/ natty-security universe\ndeb-src http://ports.ubuntu.com/ubuntu-ports/ natty-security universe\ndeb http://ports.ubuntu.com/ubuntu-ports/ natty-security multiverse\ndeb-src http://ports.ubuntu.com/ubuntu-ports/ natty-security multiverse\n" > /etc/apt/sources.list
+
+
+## Updating packages for Debian install
+
+echo " "
+echo "Updating base install package index."
+echo " "
+apt-get update
+echo " "
+echo "Setting up locales and console-data.  For english set en-us-UTF8."
+echo " "
+
+#apt-get -y install language-pack-en
+apt-get -y install locales
+#dpkg-reconfigure locales
+apt-get -y install console-data
+#dpkg-reconfigure console-data
+echo "LC_ALL=\"en_US.UTF-8\"" >> /etc/default/locale
+
+
+## Finishing touches
+
+echo " "
+echo "Installing other packages that are needed."
+echo " "
+sleep 2
+
+echo " "
+echo "Starting tasksel. . ."
+sleep 3
+
+tasksel install standard
+echo "Cleaning up install packages to save space on HDD. . ."
+apt-get clean
+
+
+
+## Installing dev packages for kernel build
+echo " "
+echo "Installing development packages for kernel build"
+echo " "
+apt-get -y install git build-essential ncurses-dev git-core
+
+
+## Creating Swap Parition and Enabling
+
+echo " "
+echo "Setting Swap Parition and Enabling."
+echo " "
+mkswap /dev/ps3dd1
+swapon /dev/ps3dd1
+
+
+## Git cloning of Kernal)
+echo "Downloading kernel source from git and creating symlink"
+cd /usr/src
+git clone --depth 1 git://git.gitbrew.org/ps3/ps3linux/linux-2.6.git || git clone --depth 1 git://foxbrew.org/ps3linux/linux-2.6.git
+ln -sf /usr/src/linux-2.6 /usr/src/linux
+cp /usr/src/linux/ps3_linux_config /usr/src/linux/.config
+
+
+## Kernel compilation
+
+echo " "
+echo "Starting compilation of kernel. (Takes around 30 mins or less.)"
+cd /usr/src/linux
+#make menuconfig
+make oldconfig
+make
+make install
+make modules_install
+cd /
+echo " "
+echo "Kernel compiling is done if no errors occured."
+echo " "
+
+
+## Creating kboot.conf entry
+
+echo " "
+echo "Creating kboot.conf entries. . ."
+echo " "
+
+E=`ls /boot | grep vmlinux`
+
+echo -e "Ubuntu=/boot/$E root=/dev/ps3dd2 noplymouth nosplash\nUbuntu_Hugepages=/boot/$E root=/dev/ps3dd2 hugepages=1 noplymouth nosplash" > /etc/kboot.conf
+
+
+## Creating /dev/ps3flash device for ps3-utils
+
+echo " "
+echo -e "Creating udev device \"ps3vflash\" for ps3-utils"
+echo " "
+echo -e "KERNEL==\"ps3vflash\", SYMLINK+=\"ps3flash\"" > /etc/udev/rules.d/70-persistent-ps3flash.rules
+
+
+## Finished
+
+echo " "
+echo "Installation is complete. Upon reboot, select your new kboot entry to boot Ubuntu."
+echo " "
+read -p "Press any key to reboot.  (If system hangs, hold power button for 8 seconds.)"
+
+echo " " 
+echo "Enjoy!"
+
+reboot
